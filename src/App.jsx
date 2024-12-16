0f3513d7285c4d882f5c3b1e563ec8a7e0692086
Kauê Alves Silva <126356237+Kaue-Alves@users.noreply.github.com>
2024-12-16 10:02:01 -0300

    implementa componente de avaliação interativa com estilos e estrutura de layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import * as S from './styles/style';
+import * as S from "./styles/style";
+import starLogo from "./assets/images/icon-star.svg";
 
 export default function App() {
-  return (
-    <>
-      <S.GlobalStyle />
-      <S.Container>
-        <S.ContainerCentro>
-          <h1>Interactive Rating Component</h1>
-        </S.ContainerCentro>
-      </S.Container>
-    </>
-  );
+    return (
+        <>
+            <S.GlobalStyle />
+            <S.Container>
+                <S.ContainerCentro>
+                    <S.Estrela>
+                        <img src={starLogo} alt="Estrela" />
+                    </S.Estrela>
+                    <S.Title>How Did We Do?</S.Title>
+                    <S.Paragrafo>
+                        Please let us now how we did with your suport request.
+                        All feedback is appreciated to help us improve our
+                        offering!
+                    </S.Paragrafo>
+
+                    <S.ContainerInputs>
+                        <S.ContainerLabelRadio>
+                            <S.RadioLabel>
+                                <S.StyledRadio name="nivel" />
+                                <S.StyleRating>1</S.StyleRating>
+                            </S.RadioLabel>
+                            <S.RadioLabel>
+                                <S.StyledRadio name="nivel" />
+                                <S.StyleRating>2</S.StyleRating>
+                            </S.RadioLabel>
+                            <S.RadioLabel>
+                                <S.StyledRadio name="nivel" />
+                                <S.StyleRating>3</S.StyleRating>
+                            </S.RadioLabel>
+                            <S.RadioLabel>
+                                <S.StyledRadio name="nivel" />
+                                <S.StyleRating>4</S.StyleRating>
+                            </S.RadioLabel>
+                            <S.RadioLabel>
+                                <S.StyledRadio name="nivel" />
+                                <S.StyleRating>5</S.StyleRating>
+                            </S.RadioLabel>
+                        </S.ContainerLabelRadio>
+
+                        <S.StyledButton>SUBMIT</S.StyledButton>
+                    </S.ContainerInputs>
+                </S.ContainerCentro>
+            </S.Container>
+        </>
+    );
 }
--- src/styles/style.js
+++ src/styles/style.js
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import styled from 'styled-components';
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   html, body, #root {
@@ -7,22 +7,118 @@ export const GlobalStyle = createGlobalStyle`
     height: 100%;
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+    font-family: 'overpass';
   }
 `;
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #141519;
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: #141519;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export const ContainerCentro = styled.div`
-  background-color: #202731;
-  width: 50%;
-  height: 50%;
-  border-radius: 10px;
+    background-color: #202731;
+    width: 80%;
+    max-width: 350px;
+    height: auto;
+    border-radius: 20px;
+    padding: 30px 30px 40px 30px;
+    display: flex;
+    flex-direction: column;
 `;
+
+export const Estrela = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #242C34;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 700;
+  font-family: 'overpass';
+`
+
+export const Paragrafo = styled.p`
+  color: #959EAC;
+  line-height: 1.5;
+  font-size: 1rem;
+  font-weight: 400;
+  font-family: 'overpass';
+`
+
+export const ContainerInputs = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`
+
+export const ContainerLabelRadio = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 20px;
+`
+
+export const RadioLabel = styled.label`
+  position: relative;
+  width: 46px;
+  height: 46px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledRadio = styled.input.attrs({ type: 'radio' })`
+  width: 100%;
+  height: 100%;
+  appearance: none;
+  background-color: #343A40;
+  border-radius: 50%; 
+  position: absolute;
+
+  &:hover {
+    cursor: pointer;
+  }
+  
+  &:checked {
+    background-color: #FB7413;
+  }
+`;
+
+export const StyleRating = styled.span`
+  font-weight: 700;
+  font-family: 'Overpass', sans-serif;
+  color: white;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.button`
+  width: 100%;
+  height: 50px;
+  background-color: #FB7413;
+  letter-spacing: 3px;
+  color: #121417;
+  font-size: 1rem;
+  font-weight: 700;
+  font-family: 'overpass';
+  border: none;
+  outline: none;
+  max-width: 300px;
+  border-radius: 50px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
